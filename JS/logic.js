925326fbf267b15a8f50a22789ff0a5a7bc16573
romek-krasnoperov <115365225+romek-krasnoperov@users.noreply.github.com>
2022-12-22 13:47:22 +0100

    Logic is created. After the game the result will be refreshed and nulled

--- JS/logic.js
+++ JS/logic.js
@@ -1,43 +1,90 @@
-function getComputerChoice() {
-    return (Math.floor(Math.random()*3) == 1) ? "Rock"
-    : (Math.floor(Math.random()*3) == 2) ? "Paper"
-    : "Scissors";
+// Accessing the buttons and the tex labels
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const tieText = document.getElementById("result-tie");
+
+let computerWinAmount = 0;
+let playerWinAmount = 0;
+let tieAmount = 0;
+
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+const signs = [ROCK, PAPER, SCISSORS];
+
+// Assign playRound to EventListener
+document.getElementById("button-container").addEventListener("click", clickListener)
+function clickListener(e) {
+    // Exit if no button was clicked
+    if (e.target.tagName !== "BUTTON") {
+        return;
+    }
+    const playerSelection = e.target.id;
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
 }
 
-//let playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-let playerScore = 1;
-let computerScore = 1;
+// get random Computer choices
+function computerPlay(){
+    return signs[(Math.floor(Math.random()*3))]
+}
+/* one with "if" statement 
+function computerPlay() {
+    return (Math.floor(Math.random() * 3) == 1) ? ROCK
+        : (Math.floor(Math.random() * 3) == 2) ? PAPER
+            : SCISSORS;
+}
+*/
+
+// Change labels + count "won"
+function playerWin() {
+    ++playerWinAmount;
+    playerScore.innerHTML = "Your score is: " + playerWinAmount;
+};
+
+function computerWin() {
+    ++computerWinAmount;
+    computerScore.innerHTML = "The computer score is: " + computerWinAmount;
+}
+
+function tieWin() {
+    ++tieAmount;
+    tieText.innerHTML = "Tie rounds: " + tieAmount;
+}
+
+// Function that checks winning very inelegantly
 
 function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice().toLocaleLowerCase();
-    playerSelection = playerSelection.toLocaleLowerCase();
     if  (playerSelection==computerSelection){
-        return "Tie Game!";
+        tieWin();
     } else if (
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper") ||
         (playerSelection == "rock" && computerSelection == "scissors")
     ) {
-        console.log("Player won this round!");
-        return playerScore++;
+        playerWin();
     } else {
-        console.log("Computer won this round!");
-        return computerScore++;
+        computerWin();
+    }
+
+    if (computerWinAmount == 5) {
+        alert("You lost! Better luck next time :(");
+        reset();
+    } else if (playerWinAmount == 5) {
+        alert("You won! Congratulations! :)")
+        reset();
     }
 }
 
-function playGame() {
-    for (let i=0; i<5; i++) {
-        let playerSelection = prompt ("Please choose by typing either: Rock, Paper or Scissors");
-        playRound(playerSelection, computerSelection);
-    }
-    
-    let winner = "Player won this game!";
-    if (playerScore<computerScore) {
-        winner = "Computer won this game!";
-    }
-    return winner;
+//Reset a game
+function reset() {
+    playerScore.innerHTML = "Your score is: 0";
+    computerScore.innerHTML = "The computer score is: 0";
+    tieText.innerHTML = "Tie rounds: 0";
+    location.reload();
 }
-
-playGame();
